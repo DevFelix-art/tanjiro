73a67b30425c9c4129f919b3a15458d70ec89f46
DevFelix-art <devfelix71@gmail.com>
2025-11-23 02:00:01 -0400

    Actualizar sockets-activos.js

--- plugins/sockets-activos.js
+++ plugins/sockets-activos.js
@@ -55,7 +55,7 @@ let handler = async (m, { conn }) => {
     }
   }
 
-  let txt = `𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪ Lista de bots activos (*${sesiones}* sesiones)\n\n❖ Principales » *${totalPrincipales}*\n✰ Subs » *${totalSubs}*\n\n`
+  let txt = `🔥 Lista de bots activos (*${sesiones}* sesiones)\n\n❖ Principales » *${totalPrincipales}*\n✰ Subs » *${totalSubs}*\n\n`
   txt += `❏ En este grupo: *${botsEnGrupo}*\n\n`
 
   if (botsEnGrupo > 0) {
