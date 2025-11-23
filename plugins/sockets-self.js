297ecd54023eb85d2eb06b7b637f65a77921257d
DevFelix-art <devfelix71@gmail.com>
2025-11-23 02:08:42 -0400

    Actualizar sockets-self.js

--- plugins/sockets-self.js
+++ plugins/sockets-self.js
@@ -1,7 +1,7 @@
 const handler = async (m, { conn, command, usedPrefix, text }) => {
   try {
     const isSubBots = [conn.user.jid, ...global.owner.map(([number]) => `${number}@s.whatsapp.net`)].includes(m.sender)
-    if (!isSubBots) return m.reply(`🤨 Esto solo puede ser usado por mi we`)
+    if (!isSubBots) return m.reply(`❖ El comando *setbanner* solo puede ser usado por el dueño del número del *bot*.`)
 
     const config = global.db.data.settings[conn.user.jid]
     const value = text ? text.trim().toLowerCase() : ''
@@ -11,11 +11,11 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
     const disable = value === 'disable' || value === 'off'
     if (enable || disable) {
       if (isEnable === enable)
-        return m.reply(`🤨 El modo *${type}* ya estaba ${enable ? 'activado' : 'desactivado'}.`)
+        return m.reply(`🔥 El modo *${type}* ya estaba ${enable ? 'activado' : 'desactivado'}.`)
       config[type] = enable
-      return conn.reply(m.chat, `👑 Has *${enable ? 'activado' : 'desactivado'}* el modo *${type}* para esta session.`, m, rcanal)
+      return conn.reply(m.chat, `🔥 Has *${enable ? 'activado' : 'desactivado'}* el modo *${type}* para esta session.`, m, rcanal)
     }
-    conn.reply(m.chat, `👑 Puedes activar o desactivar la función *${command}* utilizando:\n\n💜 *${command}* enable\n💜 *${command}* disable\n\n🛠 Estado actual » *${isEnable ? '✓ Activado' : '✗ Desactivado'}*`, m, rcanal)
+    conn.reply(m.chat, `🔥 Puedes activar o desactivar la función *${command}* utilizando:\n\n🌸 *${command}* enable\n💪 *${command}* disable\n\n🛠 Estado actual » *${isEnable ? '✓ Activado' : '✗ Desactivado'}*`, m, rcanal)
   } catch (error) {
     await m.react('✖️')
     conn.reply(m.chat, `Error:.\n\n${error.message || error}`, m, rcanal)
