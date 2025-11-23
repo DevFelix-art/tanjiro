723b578ede8c4fb787686a87e47339905600d563
DevFelix-art <devfelix71@gmail.com>
2025-11-23 02:03:57 -0400

    Actualizar sockets-leave.js

--- plugins/sockets-leave.js
+++ plugins/sockets-leave.js
@@ -1,13 +1,13 @@
 const handler = async (m, { conn, command, usedPrefix, text }) => {
   try {
     const isSubBots = [conn.user.jid, ...global.owner.map(([number]) => `${number}@s.whatsapp.net`)].includes(m.sender)
-    if (!isSubBots) return m.reply(`🤨 Eso solo puede ser usado por mi we`)
+    if (!isSubBots) return m.reply(`❖ El comando *leave* solo puede ser usado por el dueño del número del *bot*.`)
 
     await m.react('🕒')
     const id = text || m.chat
     const chat = global.db.data.chats[m.chat]
     chat.welcome = false
-    await conn.reply(id, `☹ Adiós, La bot más magica se despide de todos`)
+    await conn.reply(id, `🔥 Se les va el chico más sociable. fue un placer 💪`)
     await conn.groupLeave(id)
     chat.welcome = true
     await m.react('✔️')
