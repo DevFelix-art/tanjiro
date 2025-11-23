eeac5d77c067733b5a9167263cd05dcc35a4eff7
DevFelix-art <devfelix71@gmail.com>
2025-11-23 02:08:00 -0400

    Actualizar sockets-reload.js

--- plugins/sockets-reload.js
+++ plugins/sockets-reload.js
@@ -5,18 +5,18 @@ import fs from 'fs'
 const handler = async (m, { conn, command, usedPrefix }) => {
   try {
     const isSubBots = [conn.user.jid, ...global.owner.map(([number]) => `${number}@s.whatsapp.net`)].includes(m.sender)
-    if (!isSubBots) return m.reply(`*🤨 Este codigo solo puede ser usado por magos.*`)
+    if (!isSubBots) return m.reply(`❖ El comando *setbanner* solo puede ser usado por el dueño del número del *bot*.`)
 
     const rawId = conn.user?.id || ''
     const cleanId = jidDecode(rawId)?.user || rawId.split('@')[0]
     const sessionPath = path.join(global.jadi, cleanId)
-    if (!fs.existsSync(sessionPath)) return conn.reply(m.chat, '*👑 Este Comando solo está disponible en subs.*', m, rcanal)
+    if (!fs.existsSync(sessionPath)) return conn.reply(m.chat, '*🔥 Este Comando solo está disponible en subs.*', m, rcanal)
     await m.react('🕒')
     if (typeof global.reloadHandler !== 'function')
       throw new Error('No se encontró la función global.reloadHandler')
     await global.reloadHandler(true)
     await m.react('✔️')
-    conn.reply(m.chat, '🙃 La sesión fue recargada correctamente.', m, rcanal)
+    conn.reply(m.chat, '🔥 La sesión fue recargada correctamente.', m, rcanal)
   } catch (error) {
     await m.react('✖️')
     conn.reply(m.chat, `Error:\n\n${error.message || error}`, m, rcanal)
